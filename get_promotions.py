e5989b698fd3d2d4e244846f763f68ed5aad59db
Vitor Hugo <vtr@hotmail.com.br>
2015-07-26 19:23:08 -0300

    the adventures of fixing encoding #1

--- get_promotions.py
+++ get_promotions.py
@@ -2,12 +2,15 @@ from bs4 import BeautifulSoup as bs
 import requests
 import urllib
 import lxml.html
+import cProfile
 
+'''format:
+scrap_url, base_url, key_word'''
 
-site = 'http://adrenaline.uol.com.br/forum/sale-221'
-base_url = 'http://adrenaline.uol.com.br/forum'
-html_doc = requests.get(site)
-soup = bs(html_doc.content)
+sites = {'adrenaline': [
+    'http://adrenaline.uol.com.br/forum/sale-221', 'http://adrenaline.uol.com.br/forum', 'showthread']}
+# html_doc = requests.get(site)
+# soup = bs(html_doc.content)
 
 
 def get_title(url):
@@ -20,36 +23,38 @@ def complete_links(partial_url, base_url):
     return '{}/{}'.format(base_url, partial_url)
 
 
-def get_thread_links(scrape_url, base_url, key_word, n_links=25):
+def get_thread_links(scrap_url, base_url, key_word, n_links=10):
     '''Key_word argument to filter the threads of a given forum'''
-    links_list = set()
-    connection = urllib.urlopen(scrape_url)
+    connection = urllib.urlopen(scrap_url)
     dom = lxml.html.fromstring(connection.read())
+    links_list = set()
     for link in dom.xpath('//a/@href'):
         if key_word in link:
             link, _ = link.split('&', 1)
             links_list.add(complete_links(link, base_url))
         if len(links_list) == n_links:
             return links_list
-            # yield complete_links(link, base_url)
 
 
-
-links = get_thread_links(site, base_url, 'showthread')
-
-for link in links:
-    title = get_title(link)
-    _, thread_title = title.strip().split('>', 1)
-    if thread_title.startswith('['):
+def filter_adrenaline(scrap_url, base_url, key_word):
+    formatted_links = []
+    for link in get_thread_links(scrap_url, base_url, key_word):
+        title = get_title(link)
+        _, t = title.split('>', 1)
+        thread_title, _ = t.split('<', 1)
         print(thread_title)
+        if thread_title.startswith('aaa ['):
+            print("Promo: {} \n Link: {}".format(thread_title, link).encode('utf-8'))
+    return formatted_links
 
 
-# n_links = 0
+filter_adrenaline(
+    sites['adrenaline'][0], sites['adrenaline'][1], sites['adrenaline'][2])
 
 # while n_links < 3:
 #     thread_title = get_title(next(links))
-#     # if thread_title.startswith('['):
-#     # print (thread_title)
+# if thread_title.startswith('['):
+# print (thread_title)
 #     _, title = thread_title.split('>', 1)
 #     if title.strip().startswith('['):
 #         print(title)
