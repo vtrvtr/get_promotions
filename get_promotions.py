7cd59b6e30484d7eb9b3455f2293098b437a054a
Vitor Hugo <vtr@hotmail.com.br>
2015-07-28 20:54:12 -0300

    added txt writer

--- get_promotions.py
+++ get_promotions.py
@@ -1,14 +1,9 @@
-from bs4 import BeautifulSoup as bs
-import requests
-import urllib
 import lxml.html
-import cProfile
+import requests
+from bs4 import BeautifulSoup as bs
 
-'''format:
-scrap_url, base_url, key_word (if needed)'''
-
-sites = {'adrenaline': [
-    'http://adrenaline.uol.com.br/forum/sale-221', 'http://adrenaline.uol.com.br/forum', 'showthread'], 'hardmob': ['http://www.hardmob.com.br/promocoes', 'http://www.hardmob.com.br/promocoes', 'promocoes']}
+FILE_PATH = 'E:\\Documents\\promocoes.txt'
+FILE = open(FILE_PATH, 'w')
 
 
 def get_title(url):
@@ -22,20 +17,32 @@ def complete_links(partial_url, base_url):
 
 
 def get_dom(scrap_url):
+    '''html_link -> lxml dom'''
     html_doc = requests.get(scrap_url)
     return lxml.html.fromstring(html_doc.content)
 
 
 def get_soup(link):
+    '''html link -> BeautifulSoup soup'''
     html_doc = requests.get(link)
     return bs(html_doc.content)
 
 
 def format_adrenaline_link(link):
+    '''link (adrenaline) -> formatted link
+    '''
     title = get_title(link)
-    _, t = title.split('>', 1)
-    thread_title, _ = t.split('<', 1)
-    return "Promo: {}\nLink: {}".format(thread_title, link) if thread_title.startswith(' [') else None
+    _, title = title.split('>', 1)
+    thread_title, _ = title.split('<', 1)
+    return "\nPromo: {}\nLink: {}\n".format(thread_title, link) if thread_title.startswith(' [') else None
+
+
+def format_hardmob_links(link):
+    _, title = link.split('-', 1)
+    c, _ = title.split('.', 1)
+    formatted_title = ' '.join(
+        [word for word in c.split('-')[1:] if word is not '-'])
+    return "\nPromo: {}\nLink: {}\n".format(formatted_title.replace(' r ', ' R$').title(), link)
 
 
 def get_adrenaline_links(scrap_url='http://adrenaline.uol.com.br/forum/sale-221', base_url='http://adrenaline.uol.com.br/forum', key_word='showthread', n_links=10):
@@ -51,15 +58,10 @@ def get_adrenaline_links(scrap_url='http://adrenaline.uol.com.br/forum/sale-221'
     for link in links_list:
         corrected_link = format_adrenaline_link(link)
         if corrected_link:
-            print(corrected_link)
-
-
-def format_hardmob_links(link):
-    _, title = link.split('-', 1)
-    c, _ = title.split('.', 1)
-    formatted_title = ' '.join(
-        [word for word in c.split('-')[1:] if word is not '-'])
-    return "Promo: {}\nLink: {}".format(formatted_title.replace(' r ', ' R$').title(), link)
+            try:
+                FILE.write(corrected_link)
+            except Exception as e:
+                FILE.write(e)
 
 
 def get_hardmob_links(scrap_url='http://www.hardmob.com.br/promocoes', n_links=5):
@@ -67,9 +69,12 @@ def get_hardmob_links(scrap_url='http://www.hardmob.com.br/promocoes', n_links=5
     link_count = 0
     for link in dom.findAll('a'):
         if link.get('id'):
-            if 'thread_title' in link.get('id') and 'twitter' not in link.get('href') and 'faq' not in link.get('href'):
+            if 'thread_title' in link.get('id') and 'twitter' not in link.get('href') and 'faq' not in link.get('href') and 'descontos' not in link.get('href'):
                 thread_link = link.get('href')
-                print(format_hardmob_links(thread_link))
+                try:
+                    FILE.write(format_hardmob_links(thread_link))
+                except Exception as e:
+                    FILE.write(e)
                 link_count += 1
             if link_count == n_links:
                 break
@@ -86,12 +91,21 @@ def get_promoforum_links(scrap_url='http://www.promoforum.com.br/forums/promocoe
         if link.get('class'):
             if link.get('class')[0] == 'PreviewTooltip' and 'postar' not in link.get('href'):
                 thread_link = link.get('href')
-                print(
-                    format_hardmob_links(complete_links(thread_link, base_link)))
+                try:
+                    FILE.write(
+                        format_hardmob_links(complete_links(thread_link, base_link)))
+                except Exception as e:
+                    FILE.write(e)
                 link_count += 1
             if link_count == n_links:
                 break
 
-get_adrenaline_links()
-get_hardmob_links()
-get_promoforum_links()
+
+def main():
+    get_adrenaline_links()
+    get_hardmob_links()
+    get_promoforum_links()
+
+
+if __name__ == '__main__':
+    main()
